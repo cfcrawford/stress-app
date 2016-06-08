3db023db87f4b0b43a888a052349651d92e8802b
cfcrawford <camilafcrawford@gmail.com>
2016-06-07 19:25:44 -0700

    Edit files

--- app.rb
+++ app.rb
@@ -1,17 +1,29 @@
 class StressApp < Sinatra::Base
 
   get '/' do
-    "Hello world!"
+    @cheerup = Cheerup.all
+    erb :index
   end
 
   get '/cheerups' do
-    @cheerups = Cheerup.all
-    erb :"index"
+     @cheerups = Cheerup.all
+     erb :index
+  end
+
+  get '/new' do #or get '/cheerups/new' do
+    erb :new
+  end
+
+  post '/cheerups' do
+    p params
+    @cheerup = Cheerup.create(params[:cheerup])
+    @cheerup.save
+    redirect "/"
   end
 
   get '/cheerups/:id' do
     @cheerups = Cheerup.all
     @cheerups = Cheerup.find(params[:id])
     @cheerup.title
-    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,37 @@ Cheerup.destroy_all
 Cheerup.create([
   {
     text: "Don't worry be happy!",
-    image: "https://pbs.twimg.com/media/BWqC4tjCYAEDu76.jpg",
+    image: "src=https://pbs.twimg.com/media/BWqC4tjCYAEDu76.jpg",
     source: "Bobby McFerrin",
     category: "Worry",
     votes: "5"
   },
   {
     text: "Don't worry be happy!",
-    image: "https://pbs.twimg.com/media/BWqC4tjCYAEDu76.jpg",
+    image: "src:https://pbs.twimg.com/media/BWqC4tjCYAEDu76.jpg",
     source: "Bobby McFerrin!",
     category: "Happy",
     votes: "4"
   }
+  # {
+  #   text: "Don't let it get to you!", image: "https://pbs.twimg.com/media/BWqC4tjCYAEDu76.jpg", source: "Jane Doe", category: "Worry", votes: "19"
+  # },
+  # {
+  #   text: "Remember your pet loves you!", image: "https://pbs.twimg.com/media/BWqC4tjCYAEDu76.jpg", source: "John Doe", category: "Pet Stress", votes: "17"
+  # },
+  # {
+  #   text: "Some advice here!",
+  #   image: "src=https://pbs.twimg.com/media/BWqC4tjCYAEDu76.jpg",
+  #   source: "Bobby McFerrin",
+  #   category: "Worry",
+  #   votes: "25"
+  # },
+  # {
+  #   text: "Some stuff here!",
+  #   image: "src=https://pbs.twimg.com/media/BWqC4tjCYAEDu76.jpg",
+  #   source: "Bobby McFerrin",
+  #   category: "Worry",
+  #   votes: "3"
+  # }
+
   ])
